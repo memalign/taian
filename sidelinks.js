881f5762247f9711f557667c99a844d5506db716
Danny Pollack <dpollack@gmail.com>
2012-11-13 23:59:25 -0800

    Initial import

---
+++ sidelinks.js
@@ -0,0 +1,8 @@
+document.write('\
+<li><a href="http://windows.microsoft.com/">International Student</a></li> \
+<li><a href="http://windows.microsoft.com/">Global Medical</a></li> \
+<li><a href="http://windows.microsoft.com/">Travel Insurance</a></li> \
+<li><a href="http://windows.microsoft.com/">Texas Residents</a></li> \
+<li><a href="http://windows.microsoft.com/">Indiana Residents</a></li> \
+<li><a href="http://windows.microsoft.com/">Life Insurance</a></li> \
+');
