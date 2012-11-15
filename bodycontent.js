c95a4b7393a62e6092622613134e216743409af9
memalign <memalign@not-a-valid-address>
2012-11-14 22:55:36 -0800

    Move tabs to the side, restyle

--- bodycontent.js
+++ bodycontent.js
@@ -13,14 +13,15 @@ function writeTopLevelURL(divID, titleString) {
     document.write('<a href="#" onclick="showPivot(\''+divID+'\'); return false;" class="pivot-switch">' + loc(titleString) + '</a>');
 }
 
-document.write('<h2 bi:titleflag="t1" bi:title="t1">');
-writeTopLevelURL("pivot-international-student", "International Student");
-writeTopLevelURL("pivot-global-medical", "Global Medical");
-writeTopLevelURL("pivot-travel-insurance", "Travel Insurance");
-writeTopLevelURL("pivot-texas-indiana-residents", "Texas Residents");
-writeTopLevelURL("pivot-texas-indiana-residents", "Indiana Residents");
-writeTopLevelURL("pivot-life-insurance", "Life Insurance");
-document.write('</h2>');
-        
+function writeTabs() {
+    document.write('<h2 bi:titleflag="t1" bi:title="t1">');
+    writeTopLevelURL("pivot-international-student", "International Student");
+    writeTopLevelURL("pivot-global-medical", "Global Medical");
+    writeTopLevelURL("pivot-travel-insurance", "Travel Insurance");
+    writeTopLevelURL("pivot-texas-indiana-residents", "Texas Residents");
+    writeTopLevelURL("pivot-texas-indiana-residents", "Indiana Residents");
+    writeTopLevelURL("pivot-life-insurance", "Life Insurance");
+    document.write('</h2>');
+}
 
 
--- sidelinks.js
+++
@@ -1,8 +0,0 @@
-document.write('\
-<li><a href="http://windows.microsoft.com/">International Student</a></li> \
-<li><a href="http://windows.microsoft.com/">Global Medical</a></li> \
-<li><a href="http://windows.microsoft.com/">Travel Insurance</a></li> \
-<li><a href="http://windows.microsoft.com/">Texas Residents</a></li> \
-<li><a href="http://windows.microsoft.com/">Indiana Residents</a></li> \
-<li><a href="http://windows.microsoft.com/">Life Insurance</a></li> \
-');
