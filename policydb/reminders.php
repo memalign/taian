52bf1458887461ebd03b2facd9f847f17bee7283
memalign <memalign@not-a-valid-address>
2013-09-05 23:48:27 -0700

    Adding chinese templates

--- policydb/reminders.php
+++ policydb/reminders.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Taian Policy Database - Reminders</title>
 </head>
 
@@ -9,7 +10,7 @@
 <?php
 require_once('db.php');
 
-$importantColumns = "rowid, expiration_date_fmt, effective_date_fmt, julianday(effective_date_fmt), last_reminder_date_fmt, certificate_number, certificate_status, certificate_type, primary_insured_name, insured_name, group_name, primary_email_address, other_email_address";
+$importantColumns = "rowid, expiration_date_fmt, expiration_date, effective_date_fmt, julianday(effective_date_fmt), last_reminder_date_fmt, certificate_number, certificate_status, certificate_type, primary_insured_name, insured_name, group_name, primary_email_address, other_email_address, (julianday(\"now\")-julianday(\"expiration_date_fmt\") > -1) as too_late_to_renew, (julianday(\"expiration_date_fmt\")-julianday(\"effective_date_fmt\") > 27) as is_at_least_month";
 
 function siblingsAndEmails($dbhandle, $individual) {
     global $importantColumns;
@@ -103,6 +104,213 @@ function siblingsAndEmails($dbhandle, $individual) {
 
     return array($siblings, array_keys($emailAddressMap));
 }
+
+function emailTemplateForIndividuals($individuals) {
+    $studentARenew = <<<END
+Dear __PRIMARY_INSURED_NAME__,
+
+您购买的泰安访问学者留学生保险计划A (Student Health Advantage) （__EXPIRATION_DATE__）就要过期了。 如果您仍然满足购买该保险的条件(您的签证和身份没有变化)并且需要续保的話，请在过期日期之前提交申请以完成续保。
+
+请点击此链接申请续保：  https://purchase.imglobal.com/beginrenewal?CertificateNumber=__CERTIFICATE_NUMBER__
+
+请注意，保险期间所产生的疾病续保时就不是 pre-existing condition，而重新购买新保险就不具备这个好处。
+
+另外，续保时请选择邮寄接收方式，这样您既可收到 Email confirmation, 又可以在几天后收到IMG邮寄的保险卡。
+
+非常感谢您对我们公司的信任和支持。对我们工作的不足之处，请给我们一个提醒。把我们泰安介绍给您的家人朋友和同事是对我们工作的最大的肯定。泰安公司非常希望得到您的继续支持。
+
+再次深表感谢，祝您平安健康！
+
+Chris
+美国电话: +1 (317)318-8258 (中文), +1 (317)318-8259 (英语)
+中国电话: 950-4044-2336 (中文, 北京时间上午7-11点，直接拨号，无长途费)
+taianfinancial.com/chinese
+END;
+
+    $studentBRenew = <<<END
+Dear __PRIMARY_INSURED_NAME__,
+
+您购买的泰安访问学者留学生保险计划B(Patriot Exchange Program) （__EXPIRATION_DATE__）就要过期了。 如果您仍然满足购买该保险的条件(您的签证和身份没有变化)并且需要续保的話，请在过期日期之前提交申请以完成续保。
+
+请点击此链接申请续保： https://purchase.imglobal.com/beginrenewal?CertificateNumber=__CERTIFICATE_NUMBER__
+
+请注意，保险期间所产生的疾病续保时就不是 pre-existing condition，而重新购买新保险就不具备这个好处。
+
+另外，续保时请选择邮寄接收方式，这样您既可收到 Email confirmation, 又可以在几天后收到IMG邮寄的保险卡。
+
+非常感谢您对我们公司的信任和支持。对我们工作的不足之处，请给我们一个提醒。把我们泰安介绍给您的家人朋友和同事是对我们工作的最大的肯定。泰安公司非常希望得到您的继续支持。
+
+再次深表感谢，祝您平安健康！
+
+Chris
+美国电话: +1 (317)318-8258 (中文), +1 (317)318-8259 (英语)
+中国电话: 950-4044-2336 (中文, 北京时间上午7-11点，直接拨号，无长途费)
+taianfinancial.com/chinese
+END;
+
+    $studentBBasicRenew = <<<END
+Dear __PRIMARY_INSURED_NAME__,
+
+您购买的泰安访问学者留学生保险(__EXPIRATION_DATE__)就要过期了。 如果您仍然满足购买该保险的条件(您的签证和身份没有变化)并且需要续保的話，请在过期日期之前提交申请以完成续保。
+
+请点击此链接申请续保： https://purchase.imglobal.com/beginrenewal?CertificateNumber=__CERTIFICATE_NUMBER__
+
+请注意，购买计划B时，J1签证必须选择Standard Plan。您上次购买的 <font color="red">Basic Plan</font>不符合美国国务院对J1签证保险的标准。如果您持J1鉴证需要购买standard Plan, 请不要点击上面的续保链接。点击下面链接后点击"购买计划B", 填表时选择Standard Plan：  http://taianfinancial.com/#l=cn&p=pivot-international-student 
+
+另外，请选择邮寄接收方式，这样您既可收到 Email confirmation, 又可以在几天后收到IMG邮寄给您的保险卡。
+
+非常感谢您对我们公司的信任和支持。对我们工作的不足之处，请给我们一个提醒。把我们泰安介绍给您的家人朋友和同事是对我们工作的最大的肯定。泰安公司非常希望得到您的继续支持。
+
+再次深表感谢，祝您平安健康！
+
+Chris
+美国电话: +1 (317)318-8258 (中文), +1 (317)318-8259 (英语)
+中国电话: 950-4044-2336 (中文, 北京时间上午7-11点，直接拨号，无长途费)
+taianfinancial.com/chinese
+END;
+
+    $longTravelRenew = <<<END
+Dear __PRIMARY_INSURED_NAME__,
+
+您购买的国际旅游医疗保险（__EXPIRATION_DATE__）就要过期了。 如果您仍然满足购买该保险的条件(您的签证和身份没有变化)且还想继续购买的話，请在过期日期之前提交申请以完成续保。
+
+请点击此链接申请续保： https://purchase.imglobal.com/beginrenewal?CertificateNumber=__CERTIFICATE_NUMBER__
+
+请注意，保险期间所产生的疾病续保时就不是 pre-existing condition，而重新购买新保险就不具备这个好处。
+
+另外，续保时请选择邮寄接收方式，这样您既可收到 Email confirmation, 又可以在几天后收到IMG邮寄的保险卡。
+
+非常感谢您对我们公司的信任和支持。对我们工作的不足之处，请给我们一个提醒。把我们泰安介绍给您的家人朋友和同事是对我们工作的最大的肯定。泰安公司非常希望得到您的继续支持。
+
+再次深表感谢，祝您平安健康！
+
+Chris
+美国电话: +1 (317)318-8258 (中文), +1 (317)318-8259 (英语)
+中国电话: 950-4044-2336 (中文, 北京时间上午7-11点，直接拨号，无长途费)
+taianfinancial.com/chinese
+END;
+
+    $shortTravelRebuy = <<<END
+Dear __PRIMARY_INSURED_NAME__,
+
+您购买的国际旅游医疗保险（__EXPIRATION_DATE__）就要过期了。 如果您还需要继续购买此保险的話，请点击链接 http://taianfinancial.com/#l=cn&p=pivot-travel-insurance
+
+此保险购买一个月或一个月以上，以后您就可以按月续保，手续非常简单。保险期间所产生的疾病续保时就不是 pre-existing condition，而重新购买新保险就不具备这个好处。
+
+非常感谢您对我们公司的厚爱和支持。请转告您的亲人和朋友，我们期待着能有机会为您们服务。
+
+祝您平安健康！
+
+Chris
+美国电话: +1 (317)318-8258 (中文), +1 (317)318-8259 (英语)
+中国电话: 950-4044-2336 (中文, 北京时间上午7-11点，直接拨号，无长途费)
+taianfinancial.com/chinese
+END;
+
+    $studentAExpiresToday = <<<END
+Dear __PRIMARY_INSURED_NAME__,
+
+您购买的泰安访问学者留学生保险计划A（Student Health Advantage)今天就要过期了。 如果您仍然满足购买该保险的条件(您的签证和身份没有变化)且还想继续购买的話，请在今天点击下面的链接购买计划A（Student Health Advantage) ：
+http://taianfinancial.com/#l=cn&p=pivot-international-student
+
+如有问题请随时与我们联系。
+
+非常感谢您对我们公司的厚爱和支持。祝您平安幸福！
+
+Chris
+美国电话: +1 (317)318-8258 (中文), +1 (317)318-8259 (英语)
+中国电话: 950-4044-2336 (中文, 北京时间上午7-11点，直接拨号，无长途费)
+taianfinancial.com/chinese
+END;
+
+    $studentBExpiresToday = <<<END
+Dear __PRIMARY_INSURED_NAME__,
+
+您购买的泰安访问学者留学生保险计划B（Patriot Exchange Program)今天就要过期了。 如果您仍然满足购买该保险的条件(您的签证和身份没有变化)且还想继续购买的話，请点击下面的链接购买计划B：
+http://taianfinancial.com/#l=cn&p=pivot-international-student
+
+如有问题请随时与我们联系。
+
+非常感谢您对我们公司的厚爱和支持。祝您平安幸福！
+
+Chris
+美国电话: +1 (317)318-8258 (中文), +1 (317)318-8259 (英语)
+中国电话: 950-4044-2336 (中文, 北京时间上午7-11点，直接拨号，无长途费)
+taianfinancial.com/chinese
+END;
+
+    $travelExpiresToday = <<<END
+Dear __PRIMARY_INSURED_NAME__,
+
+您购买的国际旅游医疗保险今天就要过期了。 如果您仍然满足购买该保险的条件(您的签证和身份没有变化)且还想继续购买的話，请在今天提交申请。您可以点击下面的链接重新购买爱国者旅游医疗保险：
+http://taianfinancial.com/#l=cn&p=pivot-travel-insurance
+
+如有问题请随时与我们联系。
+
+非常感谢您对我们公司的厚爱和支持。祝您平安幸福！
+
+Chris
+美国电话: +1 (317)318-8258 (中文), +1 (317)318-8259 (英语)
+中国电话: 950-4044-2336 (中文, 北京时间上午7-11点，直接拨号，无长途费)
+taianfinancial.com/chinese
+END;
+
+    $primaryInsuredName = $individuals[0]["primary_insured_name"];
+    $expirationDate = $individuals[0]["expiration_date"];
+    $certificateNumber = $individuals[0]["certificate_number"];
+    $tooLateToRenew = (intval($individuals[0]["too_late_to_renew"]) > 0);
+    $isAtLeastMonth = (intval($individuals[0]["is_at_least_month"]) > 0);
+
+    /*
+        Student A renewal  - Start with SHA
+        Student B renewal - start with EPSN
+        Student B Basic plan renewal - start with EPBN
+        Long term travel plan renew - PATA, PATI, PPLA  plus initial term (effective date to termination date) one month or more
+        Short term travel plan rebuy (under one month can't renew) - PATA, PATI, PPLA  plus initial term (effective date to termination date) under one month
+        Student A expires today rebuy - Start with SHA
+        Student B expires today rebuy - start with EPSN
+        Long term travel rebuy - PATA, PATI, PPLA  plus initial term (effective date to termination date) one month or more
+     */
+    $renewEmail = "";
+    if (startsWith($certificateNumber, "SHA")) {
+        if ($tooLateToRenew) {
+            $renewEmail = $studentAExpiresToday;
+        } else {
+            $renewEmail = $studentARenew;
+        }
+    } elseif (startsWith($certificateNumber, "EPSN")) {
+        if ($tooLateToRenew) {
+            $renewEmail = $studentBExpiresToday;
+        } else {
+            $renewEmail = $studentBRenew;
+        }
+    } elseif (startsWith($certificateNumber, "EPBN")) {
+        $renewEmail = $studentBBasicRenew;
+    } elseif (startsWith($certificateNumber, "PATA") ||
+              startsWith($certificateNumber, "PATI") ||
+              startsWith($certificateNumber, "PPLA")) {
+        if ($isAtLeastMonth) {
+            if ($tooLateToRenew) {
+                $renewEmail = $longTravelRenew;
+            } else {
+                $renewEmail = $travelExpiresToday;
+            }
+        } else {
+            $renewEmail = $shortTravelRebuy;
+        }
+    }
+
+    $renewEmail = str_replace("__PRIMARY_INSURED_NAME__", $primaryInsuredName, $renewEmail);
+    $renewEmail = str_replace("__EXPIRATION_DATE__", $expirationDate, $renewEmail);
+    $renewEmail = str_replace("__CERTIFICATE_NUMBER__", $certificateNumber, $renewEmail);
+
+    return $renewEmail;
+}
+
+function startsWith($haystack, $needle) {
+    return $needle === "" || strpos($haystack, $needle) === 0;
+}
+
 
 $dbhandle = sqlite_open('/home1/taianfin/policy.db', 0666, $error);
 if (!$dbhandle) die ($error);
@@ -281,9 +489,6 @@ if (count($allSiblings) > 0) {
 echo "Policies with the same certificate number due for a reminder:<br />";
 echo array2table($policiesOnSameCertificate);
 
-echo "Contact email addresses: " . implode(", ", $emails) . "<br />";
-
-# Suggest an email template and try to fill it in
 
     echo <<<END
 <br />
@@ -293,6 +498,16 @@ echo "Contact email addresses: " . implode(", ", $emails) . "<br />";
 </form>
 END;
 
+
+# Suggest an email template and try to fill it in
+$emailTemplate = emailTemplateForIndividuals($policiesOnSameCertificate);
+
+echo "Copy and paste this email:<br />";
+echo "Contact email addresses: " . implode(", ", $emails) . "<br />";
+
+echo "<div id='faketextarea' style='border: 1px solid black; width:900px; overflow:auto' contenteditable>";
+echo nl2br($emailTemplate);
+echo "</div>";
 
 sqlite_close($dbhandle);
 
